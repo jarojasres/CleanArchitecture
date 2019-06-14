6335e5f91235e15f0015308230df59af1780dcbb
Julian Rojas Restrepo <julian.rojas@mvm.com.co>
2019-06-14 18:15:20 -0500

    Commands, handlers, Updated course repository, Cleaned IoC Container

--- CleanArchitecture/CleanArchitecture.Infrastructure.IoC/DepedencyContainer.cs
+++ CleanArchitecture/CleanArchitecture.Infrastructure.IoC/DepedencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.CommandHandlers;
+using CleanArchitecture.Domain.Commands;
+using CleanArchitecture.Domain.Core.Bus;
 using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infrastructure.Bus;
+using CleanArchitecture.Infrastructure.Data.Context;
 using CleanArchitecture.Infrastructure.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,11 +19,20 @@ namespace CleanArchitecture.Infrastructure.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domain InMemory MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application Layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<UniversityDBContext>();
+
+
         }
     }
 }
