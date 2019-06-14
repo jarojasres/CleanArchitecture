54d1d515938d122137ba151a2257f0587c72bb62
Julian Rojas Restrepo <julian.rojas@mvm.com.co>
2019-06-14 12:36:01 -0500

    Commit implementation of Service and repository

---
+++ CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
